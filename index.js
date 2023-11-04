a933c252c791eba64cebea584d84f808e1d33f6b
KIra166 <101987026+KIra166@users.noreply.github.com>
2023-11-04 15:57:24 +0100

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-let titles = ['g....','gr...','gra..','grav.','grave']
-let index = 0
-let interval = setInterval(() => {
-    document.title = titles[index]
-    index += 1
-    if (index === titles.length) index = 0
-}, 500)
